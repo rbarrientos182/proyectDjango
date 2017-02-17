c5ee1857a030208deb9c6c243f6dd2428cf65e49
rbarrientos182 <barrientos.isc@gmail.com>
2017-02-17 13:01:06 -0600

    creando templates en vistas

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
 
+from .models import Producto
+
 # Create your views here.
+
+def hello_world(request):
+    producto = Producto.objects.order_by('id')
+    template = loader.get_template('index.html')
+    context = {
+        'producto' : producto
+    }
+    return HttpResponse(template.render(context,request))
