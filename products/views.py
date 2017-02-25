d3d51ceab9394c36cbae19540106a0278436d0a5
rbarrientos182 <barrientos.isc@gmail.com>
2017-02-25 12:32:34 -0600

    implementacion de class based views, login y logout

--- products/views.py
+++ products/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import (
+    render,
+    get_object_or_404,
+    redirect
+)
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.models import User
+
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
 from .forms import ProductForm
 from .models import Producto
@@ -40,3 +49,27 @@ def new_product(request):
     }
 
     return HttpResponse(template.render(context, request))
+
+def auth_login(request):
+    if request.method == 'POST':
+        action = request.POST.get('action',None)
+        username = request.POST.get('username',None)
+        password = request.POST.get('password',None)
+
+        if action == 'signup':
+            user = User.objects.create_user(username=username,password=password)
+
+            user.save()
+        elif action == 'login':
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+    context = {}
+    return render(request,'login/login.html')
+
+
+class ProductList(ListView):
+    model = Producto
+
+class ProductDetail(DetailView):
+    model = Producto
